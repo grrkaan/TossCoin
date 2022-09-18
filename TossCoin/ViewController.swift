37210744d4be6dc8025792caf94297a6cad8f18c
Kaan Sercan Görür <kaangorur@hotmail.com>
2022-09-18 16:21:59 +0300

    Initial Commit

---
+++ TossCoin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TossCoin
+//
+//  Created by Kaan Sercan Görür on 18.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
